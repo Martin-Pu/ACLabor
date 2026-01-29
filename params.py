b830b3a8bab36179423b3073fb5a83c031d6c1c8
simon <simon46fsg@gmail.com>
2026-01-29 21:27:27 +0100

    parameter für das pendel

---
+++ params.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+# ==========================================================
+# Parameter aus Tabelle 1 (Labor – AC – Furuta-Pendel)
+# Alle Größen in SI-Einheiten
+# ==========================================================
+
+# Physikalische Konstanten
+g = 9.81                    # m/s^2
+
+# Geometrie
+L1 = 0.155                  # m   (155 mm)
+L2 = 0.240                  # m   (240 mm)
+lP = 0.1917                 # m   (191.7 mm)
+
+# Massen
+mS = 0.0274                 # kg  (Stabmasse, 27.4 g)
+mP = 0 # 0.0510                 # kg  (Pendelmasse, 51.0 g)
+
+# Trägheitsmomente
+J1_hat = 2.89e-2            # kg*m^2
+
+# Reibungsparameter Rotor
+mu_V1 = 50.91e-2            # N*m*s
+mu_H1 = 93.58e-3            # N*m
+
+# Reibungsparameter Pendel
+mu_V2 = 1.332e-5            # N*m*s
+mu_H2 = 3.018e-4            # N*m
+
+# Aktuatorgrenzen
+tau_max = 4.0               # N*m
+
+m2 = mP + mS
+J0_hat = J1_hat + m2*lP
+J2_hat = 1/3*mS*L2**2 + mP*lP**2
+l2 = L2/2 # für pendelmasse mP = 0
